9b0445654496b2e680492558fa920a98ac1ee036
hegek87 <hegelandka@vcu.edu>
2014-01-19 23:00:16 -0500

    Representation of things that will be useful for the raytracer

---
+++ intersection.h
@@ -0,0 +1,9 @@
+#ifndef INTER_H
+#define INTER_H
+#include "../object/shape.h"
+#include "../vector3d/vector3d.h"
+
+struct intersection{
+	Vector3d position;
+	Shape sh;
+}
---
+++ ray.h
@@ -0,0 +1,8 @@
+#ifndef RAY_H
+#define RAY_H
+#include "../vector3d/vector3d.h"
+
+struct ray{
+	Vector3d position;
+	Vector3d direction;
+}
