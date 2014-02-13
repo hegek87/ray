553f8b86bec08fe140cd0f0defe58314b6dd6978
hegek87 <hegelandka@vcu.edu>
2014-02-13 15:58:06 -0500

    Finished an EXTREMELY primitive raytracer. It simply creates a bitmap file containing a red circle and black background. No shading is supported, and no recursion is used yet

--- raytracer.cpp
+++ raytracer.cpp
@@ -1,6 +1,29 @@
 #include "raytracer.hpp"
 
+const int width(500), height(500);
+Sphere obj(Color(0xFF0000), Vector3d(width/2,height/2, 250), 290);
+Color World::traceRay(Ray r){
+	Color c(0x000000);
+	// Missed
+	if(!obj.getIntersection(r).time){
+		return c;
+	}
+	// Hit
+	return c+obj.getColor();
+	
+}
+
 int main(void){
+	Bitmap bmp(width, height);
+	World w;
+	Vector3d cam(-45, 250,250);
+	for(int i = 0; i < width; ++i){
+		for(int j = 0; j < height; ++j){
+			Vector3d dir(-cam.getX(), i-cam.getY(), j-cam.getZ());
+			bmp.setPixel(i,j, w.traceRay(Ray(cam, dir.unitVec())));
+		}
+	}
+	bmp.createBMP((char *)"RT.bmp");
 	std::cout << "IT WORKS" << std::endl;
 	return 0;
 }
--- raytracer.hpp
+++ raytracer.hpp
@@ -8,4 +8,19 @@
 #include "../color/color.h"
 #include "ray.h"
 
+struct Scene{
+	//Shape sh;
+	//Scene(Shape sh) : sh(sh) {}
+};
+
+class World{
+	private:
+		//Scene sc;
+		Vector3d cam;
+	public:
+		World() : cam(0,0,0) {}
+		//World(Scene sc, Vector3d cam): sc(sc), cam(cam) {}
+		Color traceRay(Ray);
+};
+
 #endif
