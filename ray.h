165faeb6405c9fda50ef76b7ebb758dc4da2c96e
hegek87 <hegelandka@vcu.edu>
2014-02-13 14:34:22 -0500

    Updating

--- ray.h
+++ ray.h
@@ -2,8 +2,10 @@
 #define RAY_H
 #include "../vector3d/vector3d.h"
 
-struct ray{
+struct Ray{
 	Vector3d position;
 	Vector3d direction;
+	
+	Ray(Vector3d pos, Vector3d dir) : position(pos), direction(dir) {}
 };
 #endif
