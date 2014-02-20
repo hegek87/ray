c6ae5eb3ebf545a597a8018256778281fdd35474
hegek87 <hegelandka@vcu.edu>
2014-02-20 10:57:36 -0500

    Inconsequential change. Just put the camera nearer to the origin

--- raytracer.cpp
+++ raytracer.cpp
@@ -1,7 +1,7 @@
 #include "raytracer.hpp"
 
 const int width(500), height(500);
-Sphere obj(Color(0xFF0000), Vector3d(600,2500,400), 75);
+Sphere obj(Color(0xFF0000), Vector3d(50,0,0), 75);
 Color World::traceRay(Ray r){
 	Color c(0x000000);
 	// Missed
@@ -16,11 +16,11 @@ Color World::traceRay(Ray r){
 int main(void){
 	Bitmap bmp(width, height);
 	World w;
-	Vector3d cam(-45, 250,250);
-	for(int i = 0; i < width; ++i){
-		for(int j = 0; j < height; ++j){
+	Vector3d cam(-45, 0, 0);
+	for(int i = -(width/2); i < width/2; ++i){
+		for(int j = -(height/2); j < height/2; ++j){
 			Vector3d dir(-cam.getX(), i-cam.getY(), j-cam.getZ());
-			bmp.setPixel(i,j, w.traceRay(Ray(cam, dir.unitVec())));
+			bmp.setPixel(i+(width/2),j+(height/2), w.traceRay(Ray(cam, dir.unitVec())));
 		}
 	}
 	bmp.createBMP((char *)"RT.bmp");
